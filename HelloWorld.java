71103a5d4f935827e1a1cbc1af1285d019c4f73e
liumin.0621 <liumin.0621@bytedance.com>
2023-09-20 16:48:07 +0800

    Add Java example code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+```
+
+        这是一个简单的 Java 程序，在控制台打印出 "Hello, World!"。
+        # java_demo123
